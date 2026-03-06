4e1dc52ab72be1433d1ccad3922be41630736b41
Alphavirts <mugofredrick919@gmail.com>
2026-03-06 18:03:32 +0300

    Set dashboard as homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,1 @@
-import Link from "next/link";
-
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <Link
-        href="/dashboard"
-        className="px-6 py-3 bg-blue-600 text-white rounded-lg"
-      >
-        Open Fesalice Dashboard
-      </Link>
-    </div>
-  );
-}
+export { default } from "./dashboard/page";
